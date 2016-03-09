01b165095c2617cfd9cffffe752c2cc3166577d8
Martin Dier <martin.dier@gmail.com>
2016-03-09 13:32:04 +0000

    add controller calc-ctrl

---
+++ myshopping/www/js/calc_ctrl.js
@@ -0,0 +1,12 @@
+// on button press add item value to running total on button swipe minus item value from running total
+
+angular.module('ionicApp', ['ionic'])
+
+.controller('CalcCtrl', function($scope) {
+
+
+  $scope.total=77;
+
+
+});
+
