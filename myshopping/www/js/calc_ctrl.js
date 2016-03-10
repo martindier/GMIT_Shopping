496964861c34be3e285d2a0974da8b53ddcb5b48
Martin Dier <martin.dier@gmail.com>
2016-03-10 16:36:12 +0000

    start replacing hardcoded items on index page

--- myshopping/www/js/calc_ctrl.js
+++ myshopping/www/js/calc_ctrl.js
@@ -4,6 +4,12 @@ angular.module('ionicApp', ['ionic'])
 
 .controller('CalcCtrl', function($scope) {
 
+$scope.items = [
+  {name: "Milk (1l)", price:0.85 }, 
+  {name: "Milk (2l)", price:1.45 },  
+  {name: "Sliced Loaf (White)", price:0.70 }, 
+  {name: "Sliced Loaf (Brown)", price:0.80 },
+  {name: "Sliced Loaf (White)", price:2.05 }];
 
   $scope.total=77;
 
