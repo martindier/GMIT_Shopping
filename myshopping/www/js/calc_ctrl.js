a3d843da90a2a8ed0e8b2c242d8fe5dcda4ecc84
Martin Dier <martin.dier@gmail.com>
2016-03-10 16:59:31 +0000

    add currency filter

--- myshopping/www/js/calc_ctrl.js
+++ myshopping/www/js/calc_ctrl.js
@@ -7,9 +7,9 @@ angular.module('ionicApp', ['ionic'])
 $scope.items = [
   {name: "Milk (1l)", price:0.85 }, 
   {name: "Milk (2l)", price:1.45 },  
-  {name: "Sliced Loaf (White)", price:0.70 }, 
-  {name: "Sliced Loaf (Brown)", price:0.80 },
-  {name: "Sliced Loaf (White)", price:2.05 }];
+  {name: "Sliced Loaf (Brown)", price:0.70 }, 
+  {name: "Sliced Loaf (White)", price:0.80 },
+  {name: "Tea Bags (80)", price:2.05 }];
 
   $scope.total=77;
 
