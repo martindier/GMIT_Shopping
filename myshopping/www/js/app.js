f9511fe48e03caa7cbf9657227ca245ae94d6606
Martin Dier <martin.dier@gmail.com>
2016-03-15 12:25:46 +0000

    adding tab routing - incomplete

--- myshopping/www/js/app.js
+++ myshopping/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('ionicApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,18 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+ 
+
+$stateProvider.state('tab',{
+    url:"/tab",
+    abstract:true,    
+    templateUrl: 'templates/tab.html'
+  });
+
+$urlRouterProvider.otherwise("/tab");
+
+});
+
+    
